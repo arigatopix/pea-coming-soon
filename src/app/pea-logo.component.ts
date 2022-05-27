68a0f242c0fb5128d883a00d51d01be3b2967f61
arigato <teeruch.k@gmail.com>
2022-05-27 19:03:52 +0700

    move to local

--- src/app/pea-logo.component.ts
+++ src/app/pea-logo.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pea-logo',
-  template: `
-    <img
-      src="https://www.pea.co.th/Portals/0/New-pea-logo.png?ver=2020-12-07-134117-813"
-    />
-  `,
+  template: ` <img src="/assets/pea-logo.webp" /> `,
 })
 export class PeaLogoComponent {}
