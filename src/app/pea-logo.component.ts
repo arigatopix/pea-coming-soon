79f302ba8807acbbeaa73db3de705c6bf01cdd59
arigato <teeruch.k@gmail.com>
2022-05-27 19:00:51 +0700

    pea-logo done

---
+++ src/app/pea-logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pea-logo',
+  template: `
+    <img
+      src="https://www.pea.co.th/Portals/0/New-pea-logo.png?ver=2020-12-07-134117-813"
+    />
+  `,
+})
+export class PeaLogoComponent {}
